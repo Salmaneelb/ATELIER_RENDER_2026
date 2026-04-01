7b25e9763c7641373a098554a0e15e5b72a8e5ba
Salmaneelb <salmane.elboubakkari.seb@gmail.com>
2026-04-01 15:27:14 +0200

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,21 @@
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
 
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("https://URL_DE_TON_FLASK/info")
+      .then(r => r.json())
+      .then(setData);
+  }, []);
+
+  return (
+    <div>
+      <h1>Mon App React 🚀</h1>
+      {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <p>Chargement...</p>}
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
